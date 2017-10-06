7b8e36e513c33ba2a406f979a4712867b844ceee
Michael Friedman <contact@michaeldavidfriedman.com>
2017-10-05 21:10:11 -0700

    draw snake and snack to grid

--- src/App.js
+++ src/App.js
@@ -12,31 +12,53 @@ for (let i = 0; i <= GRID_SIZE; i += 1) {
 const isBorder = (x, y) =>
   x === 0 || y === 0 || x === GRID_SIZE || y === GRID_SIZE;
 
-const getCellCs = (x, y) =>
+const getCellCs = (x, y, snake, snack) =>
   cs('grid-cell', {
     'grid-cell-border': isBorder(x, y),
+    'grid-cell-snake': x === snake.coordinate.x && y === snake.coordinate.y,
+    'grid-cell-snack': x === snack.coordinate.x && y === snack.coordinate.y,
   });
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      snake: {
+        coordinate: {
+          x: 20,
+          y: 10,
+        },
+      },
+      snack: {
+        coordinate: {
+          x: 25,
+          y: 10,
+        },
+      },
+    };
   }
   render() {
+    const { snake, snack } = this.state;
     return (
       <div className="app">
         <h1>Snake!</h1>
-        <Grid />
+        <Grid snake={snake} snack={snack} />
       </div>
     );
   }
 }
 
-const Grid = () => <div>{GRID.map(y => <Row y={y} key={y} />)}</div>;
-
-const Row = ({ y }) => (
-  <div className="grid-row">{GRID.map(x => <Cell x={x} y={y} key={x} />)}</div>
+const Grid = ({ snake, snack }) => (
+  <div>{GRID.map(y => <Row y={y} key={y} snake={snake} snack={snack} />)}</div>
 );
 
-const Cell = ({ x, y }) => <div className={getCellCs(x, y)} />;
+const Row = ({ y, snake, snack }) => (
+  <div className="grid-row">
+    {GRID.map(x => <Cell x={x} y={y} key={x} snake={snake} snack={snack} />)}
+  </div>
+);
+
+const Cell = ({ x, y, snake, snack }) => (
+  <div className={getCellCs(x, y, snake, snack)} />
+);
